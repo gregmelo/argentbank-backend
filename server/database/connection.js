f09cf4f2ab22b84cab934d606769b30abadd310b
gregmelo <gregoryvericel6@gmail.com>
2025-03-17 17:10:35 +0100

    add comments

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
 const databaseUrl =
-  process.env.DATABASE_URL
+  process.env.DATABASE_URL || 'mongodb://127.0.0.1/argentBankDB'
 
 module.exports = async () => {
   try {
